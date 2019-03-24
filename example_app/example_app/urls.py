92b175aceb4e893b0d6bc71dc4259dac1d2b7c8e
Lance Goodridge <ldgoodridge95@gmail.com>
2019-03-24 00:17:35 -0400

    Added initial source code

---
+++ example_app/example_app/urls.py
@@ -0,0 +1,12 @@
+"""
+Example app URL configuration.
+"""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('core.urls')),
+]
